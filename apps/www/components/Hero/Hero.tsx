a2b7f3b2eaa3c57e9fdbb14270f2d0bf4315a0a0
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2023-04-28 18:09:56 +0200

    chore: change triangle to svg

--- apps/www/components/Hero/Hero.tsx
+++ apps/www/components/Hero/Hero.tsx
@@ -9,11 +9,11 @@ const Hero = () => {
 
   return (
     <div className="relative">
-      <SectionContainer className="pb-0 pt-24">
+      <SectionContainer className="">
         <div className="relative">
           <div className="mx-auto">
             <div className="mx-auto max-w-2xl lg:col-span-6 lg:flex lg:items-center justify-center text-center">
-              <div className="flex flex-col items-center sm:mx-auto md:w-3/4 lg:mx-0 lg:w-full gap-4 lg:gap-8">
+              <div className="md:h-[40vh] md:min-h-[300px] md:max-h-[400px] md:py-4 flex flex-col items-center justify-center sm:mx-auto md:w-3/4 lg:mx-0 lg:w-full gap-4 lg:gap-8">
                 <div>
                   <h1
                     className="
@@ -27,7 +27,7 @@ const Hero = () => {
                       Scale to millions.
                     </span>
                   </h1>
-                  <p className="p !mb-0 mt-1.5 text-sm sm:mt-5 sm:text-base lg:text-lg">
+                  <p className="py-2 text-scale-1200 !mb-0 mt-1.5 text-sm sm:mt-5 sm:text-base lg:text-lg">
                     Supabase is an open source Firebase alternative for building secure and
                     performant Postgres backends with minimal configuration.
                   </p>
@@ -85,18 +85,33 @@ const Hero = () => {
           'absolute z-[-4] flex flex-col top-0 left-0 w-screen h-screen overflow-hidden pointer-events-none',
         ].join(' ')}
       >
-        {/* <div className="w-full h-[250px] md:h-[200px] xl:h-[100px] 2xl:h-[50px] bg-[#171717]" /> */}
         <div className="absolute w-screen h-screen">
-          <div className={['backdrop-blur', styles['triangle-shape']].join(' ')} />
+          <div className="w-full h-[200px] md:h-[250px] lg:h-[150px] xl:h-[50px] 2xl:h-0 bg-[#171717]" />
+          <div className={['2xl:-mt-48'].join(' ')}>
+            <svg
+              className={['', styles['triangle-svg']].join(' ')}
+              preserveAspectRatio="none"
+              width="100%"
+              height="100%"
+              viewBox="0 0 1680 915"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M2.16005e-05 -0.000998163L1680 0.129286V234L834.197 1068.2L-0.000248139 234.001L2.16005e-05 -0.000998163Z"
+                fill="#171717"
+              />
+            </svg>
+          </div>
         </div>
-        <div className={['w-full', styles['gradient']].join(' ')} />
       </div>
       <div
         className={[
-          'absolute top-0 left-0 w-full h-[500px] lg:h-[600px] overflow-hidden pointer-events-none',
+          'absolute top-0 left-0 w-full h-[600px] md:h-[800px] overflow-hidden pointer-events-none',
           styles['hero-container'],
         ].join(' ')}
       >
+        <div className={['w-full h-full', styles['gradient']].join(' ')} />
         <div className="absolute bottom-0 z-[-2] w-full h-full bg-gradient-to-t from-scale-100 to-transparent" />
         <div
           className={[
